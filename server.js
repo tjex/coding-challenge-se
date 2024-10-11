604db00641f77dc76379f79cdaf9b3b19647e93c
tjex <tjex@tjex.net>
2024-10-11 17:43:27 +0200

    log timestamps on server

--- server.js
+++ server.js
@@ -7,6 +7,23 @@ const server = createServer(app)
 const io = new Server(server)
 
 let count = 0
+let timestamps = []
+
+// Logs the date and time of counter increments.
+// A maximum of 5 logs are stored.
+function logCounterIncrement(bool) {
+  const datetime = new Date().toUTCString()
+  if (timestamps.length >= 5) {
+    timestamps = []
+  }
+  timestamps.push(datetime)
+
+  // enable console logging of dates and count
+  if (bool) {
+    const i = (count % 5) - 1
+    console.log(timestamps.at(i), `count: ${count}`)
+  }
+}
 
 io.on('connection', socket => {
   console.log('a user connected')
@@ -18,6 +35,7 @@ io.on('connection', socket => {
     count += amount
     // broadcast to all connected clients
     io.emit('updateCount', count)
+    logCounterIncrement(true)
   })
 })
 
